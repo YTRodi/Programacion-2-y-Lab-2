e7a0e831780ac989691e67952af38e5b0eedcc1b
Yago <yagorodi@gmail.com>
2020-03-27 03:06:09 -0300

    Ejercicio 15 en proceso

---
+++ Ejercicios de la guía/Solución_Métodos_Estáticos (11 al 15)/Ejercicio_15/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_15
+{
+    class Calculadora
+    {
+        public static double Calcular(double num1, double num2, string operacion)
+        {
+            double resultado = 0;
+            if(operacion == "+")
+            {
+                resultado = num1 + num2;
+                //return resultado;
+            }
+            if(operacion == "-")
+            {
+                resultado = num1 - num2;
+            }
+            if (operacion == "*")
+            {
+                resultado = num1 * num2;
+            }
+            if (operacion == "/")
+            {
+                //VALIDAR DIVISIONNN CON METODO VALIDAR!!
+                resultado = num1 / num2;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Ejercicios de la guía/Solución_Métodos_Estáticos (11 al 15)/Ejercicio_15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_15;
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: meter un while para que el usuario elija cuando dejar de ingresar datos.
+            double num1;
+            double num2;
+            string opcion = string.Empty;
+
+            Console.Write("Ingrese primer numero: ");
+            double.TryParse(Console.ReadLine(), out num1);
+
+            Console.Write("Ingrese segundo numero: ");
+            double.TryParse(Console.ReadLine(), out num2);
+
+            Console.Write("Elegi la opcion (+,-,*,/): ");
+            opcion = Console.ReadLine();
+            switch(opcion)
+            {
+                case "+":
+                    Console.WriteLine("La suma de los numeros es: {0}", Calculadora.Calcular(num1,num2,"+"));
+                    break;
+                case "-":
+                    Console.WriteLine("La resta de los numeros es: {0}", Calculadora.Calcular(num1, num2, "-"));
+                    break;
+
+                case "*":
+                    Console.WriteLine("La multiplicacion de los numeros es: {0}", Calculadora.Calcular(num1, num2, "*"));
+                    break;
+
+                case "/":
+                    Console.WriteLine("La division de los numeros es: {0}", Calculadora.Calcular(num1, num2, "/"));
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
