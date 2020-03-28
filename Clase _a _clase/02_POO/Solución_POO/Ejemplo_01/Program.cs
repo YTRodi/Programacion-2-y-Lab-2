f3c085f64a149dc6062c40017c71ed1504bbe0a5
Yago <yagorodi@gmail.com>
2020-03-28 03:30:02 -0300

    Clase 02 completada

---
+++ Clase _a _clase/02_POO/Solución_POO/Ejemplo_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Ejemplo_01;
+
+namespace Ejemplo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triciclo a = new Triciclo();//instancia de NombreClase.
+            Triciclo b = new Triciclo();//instancia
+
+            a.color = 10;
+            b.color = 20;
+
+            Console.WriteLine(a.GetColor());
+            Console.WriteLine(b.GetColor());
+
+
+            Console.WriteLine(b.GetCantidadRuedas());
+            a.SetCantidadRuedas("3");//es de clase. no de instancia
+            Console.WriteLine(b.GetCantidadRuedas());
+
+
+            Console.ReadKey();
+        }
+    }
+}
