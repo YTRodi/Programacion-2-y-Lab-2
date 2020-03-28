173bd26e5ee3e856eb23fffb59dbcc2edd6d79ff
Yago <yagorodi@gmail.com>
2020-03-28 03:21:46 -0300

    Ejercicio 16 terminado

---
+++ Ejercicios de la guía/Solución_Objetos (16 al 18)/Ejercicio_16/Alumno.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        Random numeroRandom = new Random();
+
+        /// <summary>
+        /// Constructor por defecto modificado.
+        /// </summary>
+        public Alumno()
+        {
+            nota1 = 0;
+            nota2 = 0;
+            notaFinal = 0;//Caso contrario se inicializara en -1...??
+            apellido = string.Empty;
+            legajo = 0;
+            nombre = string.Empty;
+        }
+
+        /// <summary>
+        /// Mediante este método accedo a los atributos private.
+        /// </summary>
+        /// <param name="paramNota1"></param>
+        /// <param name="paramNota2"></param>
+        public void Estudiar(byte paramNota1, byte paramNota2)
+        {
+            this.nota1 = paramNota1;
+            this.nota2 = paramNota2;
+        }
+
+        /// <summary>
+        /// Calcula la nota final del alumno aleatoriamente entre 0 y 10.
+        /// </summary>
+        public void CalcularFinal()
+        {
+            if(nota1 >= 4 && nota2 >=4)
+            {
+                
+                notaFinal = numeroRandom.Next(0, 10);
+            }
+            else
+            {
+                notaFinal = -1;
+            }
+        }
+
+        /// <summary>
+        /// Muestra los datos del alumno.
+        /// </summary>
+        public void Mostrar()
+        {
+            if(notaFinal != -1)
+            {
+                Console.WriteLine("Nota 1: " + nota1);
+                Console.WriteLine("Nota 2: " + nota2);
+                Console.WriteLine("NotaFinal: " + notaFinal);
+                Console.WriteLine("Apellido: " + apellido);
+                Console.WriteLine("Legajo: " + legajo);
+                Console.WriteLine("Nombre: " + nombre + "\n");
+            }
+            else
+            {
+                Console.WriteLine("Alumno desaprobado: " + nombre + "\n");
+            }
+        }
+    }
+}
--- Ejercicios de la guía/Solución_Objetos (16 al 18)/Ejercicio_16/Program.cs
+++ Ejercicios de la guía/Solución_Objetos (16 al 18)/Ejercicio_16/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ejercicio_16;
 
 namespace Ejercicio_16
 {
@@ -10,6 +11,40 @@ namespace Ejercicio_16
     {
         static void Main(string[] args)
         {
+            Alumno alumno1 = new Alumno();
+            Alumno alumno2 = new Alumno();
+            Alumno alumno3 = new Alumno();
+
+            #region Datos alumnos
+                alumno1.nombre = "Cami";
+                alumno1.apellido = "Roy";
+                alumno1.legajo = 1001;
+
+                alumno2.nombre = "Yago";
+                alumno2.apellido = "Rodi";
+                alumno2.legajo = 1002;
+
+                alumno3.nombre = "Maggie";
+                alumno3.apellido = "Zeppelin";
+                alumno3.legajo = 1003;
+            #endregion
+
+            alumno1.Estudiar(10, 6);
+            alumno1.CalcularFinal();
+            alumno1.Mostrar();
+
+            //alumno2.Estudiar(5, 2);
+            //alumno2.CalcularFinal();
+            //alumno2.Mostrar();
+
+            //alumno3.Estudiar(9,9);
+            //alumno3.CalcularFinal();
+            //alumno3.Mostrar();
+
+
+
+            Console.ReadKey();
         }
     }
 }
+
