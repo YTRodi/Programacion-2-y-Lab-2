b90992932f9f38c134460e5c9fca00522f1246d3
Yago <yagorodi@gmail.com>
2020-03-29 17:18:10 -0300

    No sale ejercicio 18

---
+++ Ejercicios de la guía/Solución_Objetos (16 al 18)/Ejercicio_18/Geometria.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Punto
+    {
+        //Solo se puede asignar un valor a un campo readonly cuando se inicializa en la declaración.
+        //ejemplo: readonly private int x = 10;
+        readonly private int x;
+        readonly private int y;
+
+        public Punto(int paramX, int paramY)
+        {
+            this.x = paramX;
+            this.y = paramY;
+        }
+
+        public int GetX()
+        {
+            return this.x;
+        }
+
+        public int GetY()
+        {
+            return this.y;
+        }
+    }
+
+    class Rectangulo
+    {
+        private float area;
+        private float perimetro;
+        Punto vertice1;
+        Punto vertice2;
+        Punto vertice3;
+        Punto vertice4;
+
+        public Rectangulo(Punto paramVertice1, Punto paramVertice3)
+        {
+            this.vertice1 = paramVertice1;
+            this.vertice3 = paramVertice3;
+            area = 0;
+            perimetro = 0;
+
+            //Calcular con vertice 1 y 3? debido a que son de tipo Punto??? 
+        }
+
+        public float getArea()
+        {
+            this.area = (vertice1.GetX() * vertice3.GetY());//base del vertice 1 por altura del vertice 3
+            //No seria vertice 1 * vertice 2?? base * altura?
+            return this.area;
+        }
+        public float getPerimetro()
+        {
+            this.perimetro = ((vertice1.GetX() + vertice3.GetY())) * 2;
+            return this.perimetro; 
+        }
+
+    }
+}
---
+++ Ejercicios de la guía/Solución_Objetos (16 al 18)/Ejercicio_18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometria;
+
+namespace PruebaGeometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero = -500;//de -500 me lo pasa a 500 positivo..
+            //Console.WriteLine("numero absoluto: " + Math.Abs(numero));
+            //Console.WriteLine("valor absoluto de -50000: " + Math.Abs(-50000));
+            Punto puntoUno = new Punto(0, 0);
+            Punto puntoDos= new Punto(20, 50);
+
+            Rectangulo rectanguloUno = new Rectangulo(puntoUno, puntoDos);
+
+            Console.WriteLine("El area del triangulo uno es: " + rectanguloUno.getArea()); 
+            //Console.WriteLine(rectanguloUno.getArea());
+            //Console.WriteLine("Ingrese area: ");
+            //int.TryParse(Console.ReadLine(), out area);
+
+            //Console.WriteLine("Ingrese perimetro: ");
+            //int.TryParse(Console.ReadLine(), out perimetro);
+
+
+            Console.ReadKey();
+        }
+    }
+}
