4a065ef519ead38670bf5f313859a6fd3c2caa2c
Yago <yagorodi@gmail.com>
2020-04-09 23:20:35 -0300

    Ejercicio integrador completado

---
+++ Ejercicios de la guía/Solucion_Repaso/Entidades/Estante.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        private int ubicacionEstante;//ID
+        private Producto[] arrayProductos;
+
+        #region Constructores
+        private Estante(int capacidad)
+        {
+            this.arrayProductos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        #endregion
+
+
+        #region Métodos
+        public Producto[] GetProductos()
+        {
+            return this.arrayProductos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Ubicacion: " + e.ubicacionEstante);
+            sb.AppendLine("Productos: ");
+
+            foreach(Producto auxProducto in e.GetProductos())//o e.arrayProductos;
+            {
+                if(!(auxProducto is null))
+                {
+                    sb.AppendLine(Producto.MostrarProducto(auxProducto));
+                }
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Operaciones
+        public static bool operator ==(Estante e, Producto p)
+        {
+            return e.arrayProductos.Contains(p);//retorna true si lo encuentra y false si no.
+            //bool retorno = false;
+            //foreach (Producto auxProducto in e.GetProductos())//recorro para ver si existen.
+            //{
+            //    if (!(auxProducto is null) && auxProducto == p)
+            //    {
+            //        retorno = true;
+            //    }
+            //}
+            //return retorno;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        //+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+            
+            if(e != p)//si el producto no se encuentra en el estante.
+            {
+                //Producto[] auxProductos = e.GetProductos();
+
+                //for (int i = 0; i < auxProductos.Length; i++)
+                for (int i = 0; i < e.arrayProductos.Length; i++)
+                {
+                    if (e.arrayProductos[i] is null)// Si es null es que me queda espacio.
+                    {
+                        e.arrayProductos[i] = p;
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if(e == p)//Si el producto se encuentra en el estante
+            {
+                //Producto[] auxProductos = e.GetProductos();
+
+                //for (int i = 0; i < auxProductos.Length; i++)//recorro los productos.
+                for (int i = 0; i < e.arrayProductos.Length; i++)//recorro los productos.
+                {
+                    if (e.arrayProductos[i] == p)//si el producto se encuentra en el listado de productos
+                    {
+                        e.arrayProductos[i] = null;
+                        break;
+                    }
+                }
+            }
+            return e;
+        }
+        #endregion
+
+    }
+}
---
+++ Ejercicios de la guía/Solucion_Repaso/Entidades/Producto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        //ctrl + k,d ordena el codigo.
+
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string paramMarca, string paramCodigo, float paramPrecio)
+        {
+            this.marca = paramMarca;
+            this.codigoDeBarra = paramCodigo;
+            this.precio = paramPrecio;
+        }
+
+        #region Métodos
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Marca: " + p.marca);//console.writeline (parecido).
+            sb.AppendLine("Precio: " + p.precio);
+            sb.AppendLine("Precio: " + p.precio);
+            return sb.ToString();
+        }
+        #endregion
+
+
+        #region Conversiones
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        #endregion
+
+
+        #region Operaciones
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            return (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+        }
+        public static bool operator ==(Producto p, string paramMarca)
+        {
+            return (p.marca == paramMarca);
+        }
+
+        public static bool operator !=(Producto p, string paramMarca)
+        {
+            return !(p.marca == paramMarca);
+        }
+        #endregion
+    }
+}
