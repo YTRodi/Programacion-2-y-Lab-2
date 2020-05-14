2557a5b28dac681bdff08e3af5550d8e161dfbb3
Yago <yagorodi@gmail.com>
2020-05-14 01:07:01 -0300

    Ejercicio con manejo de InnerExceptions terminado

---
+++ Ejercicios de la guía/Excepciones/Solucion_Excepciones_ejercicio_42/Entidades/Excepciones/MiExcepcion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Excepciones
+{
+    public class MiExcepcion : Exception
+	{
+		#region Constructores
+		public MiExcepcion(string mensajeAMandar) : base(mensajeAMandar)
+		{
+		}
+		public MiExcepcion(string mensajeAMandar, Exception innerException) : base(mensajeAMandar, innerException)
+		{
+			//INER = DE DONDE VIENE EL ERROR, ES UN PASA MANOS.
+		}
+		#endregion
+	}
+}
