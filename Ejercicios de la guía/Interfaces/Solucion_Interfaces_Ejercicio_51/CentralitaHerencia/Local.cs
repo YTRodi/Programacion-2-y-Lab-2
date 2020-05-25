82a94e2922e7277189a6a585d59606ec14fcb8a0
Yago <yagorodi@gmail.com>
2020-05-25 00:48:00 -0300

    Ejercicios de la guía/Interfaces/Solucion_Interfaces_Ejercicio_51/

---
+++ Ejercicios de la guía/Interfaces/Solucion_Interfaces_Ejercicio_51/CentralitaHerencia/Local.cs
@@ -0,0 +1,90 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local<T> : Llamada, IGuardar<T>
+    {
+        protected float costo;
+
+        #region Properties
+        public override float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+        public Local(Llamada pLlamada, float pCosto) 
+            : this(pLlamada.NroOrigen, pLlamada.Duracion, pLlamada.NroDestino, pCosto)
+        {
+
+        }
+
+        public Local(string pOrigen,float pDuracion, string pDestino, float pCosto) 
+            : base(pDuracion, pDestino, pOrigen)
+        {
+            this.costo = pCosto;
+        }
+        #endregion
+
+        #region Métodos
+        private float CalcularCosto()
+        {
+            return base.duracion * this.costo;
+        }
+        #endregion
+
+        #region Override
+        public override bool Equals(object obj)
+        {
+            return (obj is Local<T>);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("Tipo de llamada: " + this.GetType());
+            sb.AppendLine("Costo de la llamada: $" + this.CostoLlamada);
+            //sb.AppendLine("Costo: " + this.costo);
+
+            return sb.ToString();
+        }
+
+
+        #endregion
+        #region Interface
+        public string RutaDeArchivo
+        {
+            get
+            {
+                return "Ruta Archivo";
+            }
+            set
+            {
+
+            }
+        }
+
+        public bool Guardar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Leer()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
