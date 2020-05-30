bf8770bacb13cd7e5154327a0f269afa953322b9
Yago <yagorodi@gmail.com>
2020-05-30 17:12:36 -0300

    Serialización en XML de las llamadas Locales y Provinciales de Centralita

---
+++ Ejercicios de la guía/Serialización/Solucion_Serialización_Ejercicio_59/CentralitaHerencia/Local.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using Interfaces;
+
+namespace CentralitaHerencia
+{
+    public class Local : Llamada, IGuardar<Local>
+    {
+        public float costo;
+        private string pathLocal;
+
+        #region Properties
+        public override float CostoLlamada
+        {
+            get
+            {
+                //return this.CalcularCosto();
+                return base.duracion * this.costo;//XMLSERIALIZER NO SERIALIZA MÉTODOS
+            }
+        }
+
+        #endregion
+
+        #region Constructores
+        public Local()
+        {
+
+        }
+        public Local(Llamada pLlamada, float pCosto)
+            : this(pLlamada.NroOrigen, pLlamada.Duracion, pLlamada.NroDestino, pCosto)
+        {
+
+        }
+
+        public Local(string pOrigen, float pDuracion, string pDestino, float pCosto)
+            : base(pDuracion, pDestino, pOrigen)
+        {
+            this.costo = pCosto;
+        }
+
+        #endregion
+
+        #region Métodos
+        private float CalcularCosto()
+        {
+            return base.duracion * this.costo;
+        }
+        #endregion
+
+        #region Override
+        public override bool Equals(object obj)
+        {
+            return (obj is Local);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("Tipo de llamada: " + this.GetType());
+            sb.AppendLine("Costo de la llamada: $" + this.CostoLlamada);
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Interface
+        public string RutaDeArchivo
+        {
+            get { return this.pathLocal; }
+            set { this.pathLocal = value; }
+        }
+
+        public bool Guardar()
+        {
+            bool pudoSerializar = false;
+            XmlTextWriter xmlWriter;
+            XmlSerializer xmlSer;
+
+            if (!(this is null) && this is Local && !(this.RutaDeArchivo is null))
+            {
+                if (Path.GetExtension(this.RutaDeArchivo) is ".xml")
+                {
+                    using (xmlWriter = new XmlTextWriter(this.RutaDeArchivo, Encoding.UTF8))
+                    {
+                        xmlSer = new XmlSerializer(typeof(Local));
+                        xmlSer.Serialize(xmlWriter, this);
+                        pudoSerializar = true;
+                    }
+                }
+            }
+            else
+                throw new InvalidCastException("No es una llamada Local");
+            return pudoSerializar;
+        }
+
+        public Local Leer()
+        {
+            Local oLocalRetorno = new Local();
+            XmlTextReader xmlReader;
+            XmlSerializer xmlSer;
+
+            if (!(this is null) && this is Local && !(this.RutaDeArchivo is null))
+            {
+                if (Path.GetExtension(this.RutaDeArchivo) is ".xml")
+                {
+                    using (xmlReader = new XmlTextReader(this.RutaDeArchivo))
+                    {
+                        xmlSer = new XmlSerializer(typeof(Local));
+                        oLocalRetorno = (Local)xmlSer.Deserialize(xmlReader);
+                    }
+                }
+            }
+            else
+                throw new InvalidCastException("No es una llamada Local");
+            return oLocalRetorno;
+        }
+        #endregion
+    }
+}
---
+++ Ejercicios de la guía/Serialización/Solucion_Serialización_Ejercicio_59/PruebaConsola/Program.cs
@@ -0,0 +1,60 @@
+﻿using CentralitaHerencia;
+using Excepciones;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Centralita c = new Centralita("Yago Center");
+
+            Local l1 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Provincial l2 = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+            Local l3 = new Local("Lanús", 45, "San Rafael", 1.99f);
+            Provincial l4 = new Provincial("Quilmes", Provincial.Franja.Franja_3, 12, "CABA");
+
+            c.RutaDeArchivo = Directory.GetCurrentDirectory() + @"\LogLlamadas.txt";
+            l1.RutaDeArchivo = Directory.GetCurrentDirectory() + @"\llamadasLocales.xml";
+            l3.RutaDeArchivo = l1.RutaDeArchivo;
+            l2.RutaDeArchivo = Directory.GetCurrentDirectory() + @"\llamadasProvinciales.xml";
+            l4.RutaDeArchivo = l2.RutaDeArchivo;
+
+            try
+            {
+                //Locales.
+                c += l1;
+                c += l3;
+                if(l1.Guardar() && l3.Guardar())
+                    Console.WriteLine("Se guardaron las llamadas Locales en llamadasLocales.xml");
+
+                //Provinciales.
+                c += l2;
+                c += l4;
+                if (l2.Guardar() && l4.Guardar())
+                    Console.WriteLine("Se guardaron las llamadas Provinciales en llamadasProvinciales.xml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("--------------llamadasLocales.xml--------------");
+            Console.WriteLine(l1.Leer().ToString());
+            Console.WriteLine(l3.Leer().ToString());
+
+            Console.WriteLine("--------------llamadasProvinciales.xml--------------");
+            Console.WriteLine(l2.Leer().ToString());
+            Console.WriteLine(l4.Leer().ToString());
+            //Console.WriteLine(c.Leer());
+
+
+            Console.ReadKey();
+        }
+    }
+}
